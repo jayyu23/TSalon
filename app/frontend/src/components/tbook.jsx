d6c7615a926ca63bb195c3cefbc32c7815b76fc1
jayyu <jayyu2011@hotmail.com>
2022-06-04 17:41:55 -0700

    Props and States. Added in API support

--- app/frontend/src/components/tbook.jsx
+++ app/frontend/src/components/tbook.jsx
@@ -4,36 +4,34 @@ class TBook extends Component {
     constructor(props) {
       super(props)
       this.state = {
-        title: "TSalon Manifesto",
-        blurb: "TSalon is a Web 3.0 publishing house and literary salon, providing readers with quality-assured NFT essays called TBooks",
+        defaultTitle: "TSalon Publication",
+        defaultBlurb: "TSalon is a Web 3.0 publishing house and literary salon, providing readers with quality-assured NFT essays called TBooks",
+        defaultAuthor: "Anonymous",
+        defaultLink: "#"
       }
     };
-    //state = {  }
-    
-    TBookTitle = "TSalon Manifesto";
-    TBookBlurb = "TSalon is a Web 3.0 publishing house and literary salon, providing readers with quality-assured NFT essays called TBooks";
-    TBookAuthor = "Jay Yu";
-    images = ['blue', 'green', 'orange', 'purple']
 
+    images = ['blue', 'green', 'orange', 'purple']
     imageCover = `/assets/logo_square_${this.images[Math.floor(Math.random() * this.images.length)]}.png`
 
-    cardHTML = (<div className="card mx-3" style={{width: "25rem"}}>
-    <img className="card-img-top" src={this.imageCover} alt="TBook Image Cap"></img>
-    <div className="card-body">
-      <h5 className="card-title">{this.TBookTitle}</h5>
-      <p className="card-text">{this.TBookBlurb}</p>
-      <p className="card-text font-italic">By {this.TBookAuthor}</p>
-      <span className="justify-content-center px-5">
-          <a href="#" className="btn btn-primary m-2">Read</a>
-          <a href="#" className="btn btn-success m-2">Collect</a>
-      </span>
-      
-      
-    </div>
-  </div>)
+    cardHTML() {
+      let card = (<div className="card mx-3" style={{width: "25rem"}}>
+        <img className="card-img-top" src={(this.props.imageCover) ? this.props.imageCover : this.imageCover} alt="TBook Image Cap"></img>
+        <div className="card-body d-flex flex-column">
+          <h5 className="card-title">{(this.props.title) ? this.props.title : this.state.defaultTitle}</h5>
+          <p className="card-text">{(this.props.blurb) ? this.props.blurb : this.state.defaultBlurb}</p>
+          <p className="card-text font-italic">By {(this.props.author) ? this.props.author : this.state.defaultAuthor}</p>
+          <span className="px-5 mt-auto">
+              <a href={(this.props.link) ? this.props.link : this.state.defaultLink} className="btn btn-primary m-2 px-auto">Read</a>
+              <a href="#" className="btn btn-success m-2 px-auto">Collect</a>
+          </span>
+        </div>
+      </div>)
+    return card;
+    }
 
-    render() { 
-        return (this.cardHTML);
+    render() {
+        return (this.cardHTML());
     }
 }
  
--- app/frontend/src/components/tbookstore.jsx
+++ app/frontend/src/components/tbookstore.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import TBook from './tbook';
 
+const apiContent = [ {id: 1, title: "Wittgenstein’s Clockface Problem", blurb: "Article 1 blurb", author: "Jay Yu", link: "https://www.google.com"},
+                    {id: 2, title: "The Blue Book", blurb: "Article 2 blurb", author: "Jay Yu", link: "https://www.google.com"},
+                    {id: 3, title: "The Buddhas Dao and Platos Li", blurb: "Article 3 blurb", author: "Jay Yu", link: "https://www.google.com"},
+                    {id: 4, title: "陋室铭", blurb: "Article 4 blurb", author: "Jay Yu", link: "https://www.google.com"},]
+
+
 class TBookStore extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
     //state = {  } 
     
     render() { 
@@ -9,14 +19,11 @@ class TBookStore extends Component {
             <div>
                 <h1 className="text-center">TBookstore</h1>
                 <div className="row justify-content-center p-3">
-                    <TBook/>
-                    <TBook/>
-                    <TBook/>
-                    <TBook/>
-                    <TBook/>
-                    <TBook/>
-                    <TBook/>
-                    <TBook/>
+                    {
+                        apiContent.map((data) => (
+                            <TBook key={data.id} title={data.title} blurb={data.blurb} author={data.author} link={data.link}/>
+                        ))
+                    }
                 </div>
             </div>
         );
