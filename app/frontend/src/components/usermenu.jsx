ce7274e55c0eea1bd21c75f62f597767ebceb941
jayyu <jayyu2011@hotmail.com>
2022-06-10 06:56:46 -0700

    Added Save Preview to Editor, simple user menu

--- app/frontend/src/components/usermenu.jsx
+++ app/frontend/src/components/usermenu.jsx
@@ -7,33 +7,33 @@ const sidebar_options = [
   { index: 2, text: "TBookshelf", link: "#" },
 ];
 
-var active = 1;
+var active = 0;
 
 class UserMenu extends Component {
   //state = {  }
   generateSidebar() {
+    let currentActive = this.props.active ? this.props.actives : active;
     let sidebarBase = (
       <div>
-        <NavBar />
         <div
-          className="d-flex flex-column flex-shrink-0 p-3 text-white bg-secondary vh-100"
-          style={{ width: 280, height: "100%" }}
+          className="d-flex flex-column flex-shrink-0 p-3 bg-light h-100"
+          style={{ width: 280, minHeight: "100pct" }}
         >
           <a
             href="/"
-            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none w-100"
+            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none w-100 h-100"
           >
-            <span className="fs-4 py-4 h3 mt-4">Dashboard</span>
+            <span className="fs-4 py-4 h3 mt-4 h-100">Dashboard</span>
           </a>
-          <ul className="nav nav-pills flex-column mb-auto">
+          <ul className="nav nav-pills flex-column mb-auto h-100">
             {sidebar_options.map((data) => (
               <li key={data.index} className="nav-item my-2">
                 <a
                   href={data.link}
                   className={
-                    data.index === active
+                    data.index === currentActive
                       ? "nav-link active"
-                      : "nav-link text-white"
+                      : "nav-link"
                   }
                   aria-current="page"
                 >
--- app/frontend/src/pages/editor.jsx
+++ app/frontend/src/pages/editor.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import SunEditor from "suneditor-react";
 import "suneditor/dist/css/suneditor.min.css"; // Import Sun Editor's CSS File
 import NavBar from "../components/navbar";
+import UserMenu from "../components/usermenu";
 
 class TSalonEditor extends Component {
   // state = {  }  <MUIRichTextEditor/>
@@ -14,64 +15,82 @@ class TSalonEditor extends Component {
     return (
       <div>
         <NavBar />
-        <h1 className="my-5 text-center">TSalon Editor</h1>
-        <div className="form-group container-fluid px-5 py-4 row">
-          <div className="col-lg-4">
-            <h4 className="font-weight-normal">Image Cover</h4>
-            <img src={this.imgUrl} alt="Cover Image" height="300" width="300" />
-            <input
-              className="mt-1"
-              type="file"
-              accept="image/png"
-              disabled="true"
-            />
-            <p className="text-muted mb-3 mt-2">300x300 PNG</p>
+        <div className="row container-fixed h-100">
+          <div className="col-3 h-100">
+            <UserMenu active={1} />
           </div>
-          <div className="col-lg-8">
-            <h4 className="font-weight-normal">Title</h4>
-            <input
-              className="form-control"
-              style={{ fontSize: 32, fontWeight: "bold" }}
-            ></input>
-            <h4 className="font-weight-normal mt-5">Blurb</h4>
-            <textarea
-              className="form-control"
-              rows="4"
-              maxLength={this.blurbLength}
-            ></textarea>
-            <p className="text-muted mt-1">
-              Max Length: {this.blurbLength} characters
-            </p>
+          <div className="col-9">
+            <h1 className="my-5 pt-5 text-center">TSalon Editor</h1>
+            <div className="form-group container-fluid px-5 py-4 row">
+              <div className="col-xl-4">
+                <h4 className="font-weight-normal">Image Cover</h4>
+                <img
+                  src={this.imgUrl}
+                  alt="Cover Image"
+                  height="300"
+                  width="300"
+                />
+                <input
+                  className="mt-1"
+                  type="file"
+                  accept="image/png"
+                  disabled="true"
+                />
+                <p className="text-muted mb-3 mt-2">300x300 PNG</p>
+              </div>
+              <div className="col-xl-8">
+                <h4 className="font-weight-normal">Title</h4>
+                <input
+                  className="form-control"
+                  style={{ fontSize: 32, fontWeight: "bold" }}
+                ></input>
+                <h4 className="font-weight-normal mt-5">Blurb</h4>
+                <textarea
+                  className="form-control"
+                  rows="4"
+                  maxLength={this.blurbLength}
+                ></textarea>
+                <p className="text-muted mt-1">
+                  Max Length: {this.blurbLength} characters
+                </p>
+              </div>
+            </div>
+            <div className="px-5">
+              <h4 className="font-weight-normal pb-2">Content</h4>
+              <SunEditor
+                height="800px"
+                width="100%"
+                setDefaultStyle="font-family: Arial; font-size: 20px;"
+                defaultValue="Start your next masterpiece here..."
+                setOptions={{
+                  buttonList: [
+                    [
+                      "formatBlock",
+                      "paragraphStyle",
+                      "bold",
+                      "underline",
+                      "italic",
+                      "strike",
+                      "list",
+                      "blockquote",
+                      "align",
+                      "font",
+                      "fontColor",
+                      "fontSize",
+                      "undo",
+                      "redo",
+                    ],
+                  ],
+                }}
+              />
+            </div>
+            <div className="row justify-content-center my-5">
+              <button className="btn btn-primary col-3 mx-3">Save</button>
+              <button type="submit" className="btn btn-primary col-3 mx-3">
+                Preview
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="px-5">
-          <h4 className="font-weight-normal pb-2">Content</h4>
-          <SunEditor
-            height="800px"
-            width="100%"
-            setDefaultStyle="font-family: Arial; font-size: 20px;"
-            defaultValue="Start your next masterpiece here..."
-            setOptions={{
-              buttonList: [
-                [
-                  "formatBlock",
-                  "paragraphStyle",
-                  "bold",
-                  "underline",
-                  "italic",
-                  "strike",
-                  "list",
-                  "blockquote",
-                  "align",
-                  "font",
-                  "fontColor",
-                  "fontSize",
-                  "undo",
-                  "redo",
-                ],
-              ],
-            }}
-          />
         </div>
       </div>
     );
