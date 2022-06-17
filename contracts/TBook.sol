e8b65bff22889da83a71092c434160d0623759a6
jayyu <jayyu2011@hotmail.com>
2022-06-16 22:49:33 -0700

    Added data structures for Solidity

--- contracts/TBook.sol
+++ contracts/TBook.sol
@@ -5,5 +5,44 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TBookFactory is Ownable, ERC721 {
+    // A Copy of a Book
+    struct CopyInfo {
+        uint256 tbsn;
+        uint256 copyNumber;
+        uint256 numTransactions; // How many times sold/transacted
+        string title;
+        address initHolder; // Who minted this
+        address currentHolder;
+        address lastHolder; // Who was the last person to hold this NFT
+    }
+    // A Single Book
+    struct TBookInfo {
+        uint256 tbsn;
+        string title;
+        uint256 numCopies;
+        uint256 mintPrice;
+        address author;
+        mapping(address => CopyInfo) addressToCopy; // Maps the holder to the copy
+        mapping(uint256 => address) copyToAddress; // Maps copy number back to the address
+    }
+    // A Collection of Books
+    struct TBookCollection {
+        string collectionName; // Optional descriptor
+        uint256 collectionSize;
+        mapping(uint256 => TBookInfo) books; // Maps TBSN to TBookInfo struct (which in turn maps to copyHolders info)
+    }
+    // A Single User
+    struct UserInfo {
+        address userAddress;
+        uint256 balance;
+        string userName;
+        TBookCollection books;
+    }
+
+    uint256 numUsers;
+    mapping(address => UserInfo) allUsers;
+
+    TBookCollection allBooks;
+
     constructor() ERC721("TBook", "TBK") {}
 }
