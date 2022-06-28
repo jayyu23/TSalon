0555855d541341473dbd5155ca6580abc8322675
jayyu <jayyu2011@hotmail.com>
2022-06-27 23:22:47 -0700

    Sophisticated Authhandler

--- app/frontend/src/auth/authhandler.js
+++ app/frontend/src/auth/authhandler.js
@@ -1,10 +1,35 @@
+import axios from "axios";
+
 class AuthHandler {
   constructor() {}
+
+  getPostAuthData() {
+    // Assume that have the Session Storage Data
+    const token = sessionStorage.getItem("t");
+    let body = { walletAddress: sessionStorage.getItem("address") };
+    let config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    return { body: body, config: config };
+  }
 
   protectRoute() {
     const token = sessionStorage.getItem("t");
     if (!token) {
       auth.redirectToError();
+    } else {
+      // ping the auth-checker backend
+      const authCheckerAPI = "http://localhost:8000/api/auth";
+      authData = this.getPostAuthData();
+      axios.post(authCheckerAPI, authData.body, authData.config).then(
+        (acc) => {
+          // All OK
+          return;
+        },
+        (rej) => {
+          window.location.href = "/error";
+        }
+      );
     }
   }
 
