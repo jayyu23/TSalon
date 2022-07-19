0a6d0ebd5beba544420d6e7d5d4fe34ad40bfba5
jayyu <jayyu2011@hotmail.com>
2022-07-19 15:50:07 -0700

    Updated poster

--- app/frontend/src/components/poster.jsx
+++ app/frontend/src/components/poster.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 
 function Poster(props) {
-  const imgUrl = "/assets/cafedeflore.jpg";
+  const imgUrl = "/assets/cafedeflore_dark.jpg";
   const logoURL = "/assets/logo_circle.png";
   return (
     <div>
       <div
-        className="vh-100 mt-0 p-0 justify-content-center pb-3"
-        style={{ backgroundImage: `url(${imgUrl})` }}
+        className="mt-0 p-0 justify-content-center pb-3"
+        style={{
+          backgroundImage: `url(${imgUrl})`,
+          backgroundSize: "cover",
+          height: window.innerHeight,
+        }}
       >
         <div className="w-100 justify-content-center text-center mt-5 pt-5">
           <img src={logoURL} alt="" />
@@ -15,16 +19,42 @@ function Poster(props) {
         <h1 className="text-white text-center mt-0" style={{ fontSize: 90 }}>
           TSalon
         </h1>
+        <div className="justify-content-center text-center mt-2 mb-5">
+          <a
+            href="https://trapezoidal-nephew-39b.notion.site/TSalon-Whitepaper-9d27f5c901bc4dcaa53bace710421fce"
+            target="_blank"
+            className="btn btn-outline-light mx-2 px-0 text-center"
+            style={{ borderRadius: 100, width: 75 }}
+          >
+            <i
+              class="fa-solid fa-file-lines"
+              style={{ fontSize: 25, width: 25 }}
+            ></i>
+          </a>
+          <a
+            href="https://discord.gg/jABTq5RPNC"
+            className="btn btn-outline-light mx-2 px-0 justify-content-center text-center"
+            style={{ borderRadius: 100, width: 75 }}
+            target="_blank"
+          >
+            <i
+              class="fa-brands fa-discord text-center mx-0 justify-content-center"
+              style={{ fontSize: 25, width: 35 }}
+            ></i>
+          </a>
+        </div>
+
         <p
-          className="subheading my-4 text-white text-center"
+          className="subheading mt-5 pt-3 text-white text-center"
           style={{ fontSize: 36, fontFamily: "Avenir" }}
         >
           A Web 3.0 Publishing House
         </p>
-        <div className="justify-content-center text-center">
+
+        <div className="justify-content-center text-center pb-5">
           <a
             href="#store"
-            className="btn btn-success my-5 ml-5 px-5"
+            className="btn btn-success ml-5 px-5"
             style={{
               fontSize: 28,
               fontFamily: "Avenir",
