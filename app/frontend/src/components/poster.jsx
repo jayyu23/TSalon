3aa164e66a90fae7293abafc3f945acd2acdd7d5
jayyu <jayyu2011@hotmail.com>
2022-07-24 16:26:38 -0700

    Change the cover

--- app/frontend/src/components/poster.jsx
+++ app/frontend/src/components/poster.jsx
@@ -16,7 +16,7 @@ function Poster(props) {
         <div className="w-100 justify-content-center text-center mt-5 pt-5">
           <img src={logoURL} alt="" />
         </div>
-        <h1 className="text-white text-center mt-0" style={{ fontSize: 80 }}>
+        <h1 className="text-white text-center mt-0" style={{ fontSize: 65 }}>
           TSalon
         </h1>
         <div className="justify-content-center text-center mt-2 mb-3">
@@ -24,29 +24,29 @@ function Poster(props) {
             href="https://trapezoidal-nephew-39b.notion.site/TSalon-Whitepaper-9d27f5c901bc4dcaa53bace710421fce"
             target="_blank"
             className="btn btn-outline-light mx-2 px-0 text-center"
-            style={{ borderRadius: 100, width: 75 }}
+            style={{ borderRadius: 100, width: 60 }}
           >
             <i
               class="fa-solid fa-file-lines"
-              style={{ fontSize: 25, width: 25 }}
+              style={{ fontSize: 18, width: 25 }}
             ></i>
           </a>
           <a
             href="https://discord.gg/jABTq5RPNC"
             className="btn btn-outline-light mx-2 px-0 justify-content-center text-center"
-            style={{ borderRadius: 100, width: 75 }}
+            style={{ borderRadius: 100, width: 60 }}
             target="_blank"
           >
             <i
               class="fa-brands fa-discord text-center mx-0 justify-content-center"
-              style={{ fontSize: 25, width: 35 }}
+              style={{ fontSize: 18, width: 35 }}
             ></i>
           </a>
         </div>
 
         <p
           className="subheading mt-3 pt-3 text-white text-center"
-          style={{ fontSize: 36, fontFamily: "Avenir" }}
+          style={{ fontSize: 24, fontFamily: "Avenir" }}
         >
           A Web 3.0 Publishing House
         </p>
@@ -62,7 +62,10 @@ function Poster(props) {
             }}
           >
             Explore
-            <i className="fa fa-arrow-right px-2" style={{ fontSize: 25 }}></i>
+            <i
+              className="fa fa-arrow-right px-2 mb-auto"
+              style={{ fontSize: 18 }}
+            ></i>
           </a>
         </div>
       </div>
